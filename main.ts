5dfe91ee5d1216d02aaaec3fc1bc9447e913335d
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2022-03-13 17:49:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
 basic.forever(function () {
     if (!(crickit.signal1.digitalRead()) && !(crickit.signal8.digitalRead())) {
-        crickit.motor1.run(-50)
-        crickit.motor2.run(-50)
+        crickit.motor1.run(-32)
+        crickit.motor2.run(-30)
         basic.showString("G")
     } else {
         if (crickit.signal1.digitalRead()) {
-        	
+            basic.pause(100)
+            crickit.motor1.run(20)
+            crickit.motor2.run(-20)
+            basic.showString("L")
         }
         if (crickit.signal8.digitalRead()) {
-        	
+            basic.pause(100)
+            crickit.motor1.run(-20)
+            crickit.motor2.run(20)
+            basic.showString("R")
         }
     }
 })
